afd22e942b7c9fbd2b2ecea3fd377ddc8adf965c
Laptev S <sl260401@gmail.com>
2023-04-11 14:38:31 +0300

    add JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,1 @@
-"use strict";
+"use strict";let message=document.getElementById("message-input");function getMessage(){console.log(message.value),document.getElementById("message-output").innerHTML=message.value,message.value=""}message.addEventListener("keydown",(function(e){"Enter"==e.key&&getMessage()}));
